27eefe9f024d97b14ba56696e8c1b848199c0173
Oliver Krška <oliverkrska@gmail.com>
2022-09-20 21:00:40 +0200

    Update Checker.cs

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_03._11
 {
@@ -6,7 +6,17 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
+            {
+                string first = str.Substring(0, str.Length / 2);
+                char[] arr = str.ToCharArray();
+
+                Array.Reverse(arr);
+
+                string temp = new string(arr);
+                string second = temp.Substring(0, temp.Length / 2);
+
+                return first.Equals(second);
+            }
         } 
     }
 }
