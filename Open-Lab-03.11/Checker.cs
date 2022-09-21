73ad9f1bdb6d52b6a306de3c5ee450614e55e541
Pho3nixG0D <oliverkrskagmail.com>
2022-09-21 21:37:05 +0200

    Dividing strin to substrings, reversing, comparing

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -5,8 +5,17 @@ namespace Open_Lab_03._11
     public class Checker
     {
         public bool IsPalindrome(string str)
-        {
-            throw new NotImplementedException();
-        } 
+        
+            {
+                string first = str.Substring(0, str.Length / 2);
+                char[] arr = str.ToCharArray();
+
+                Array.Reverse(arr);
+
+                string temp = new string(arr);
+                string second = temp.Substring(0, temp.Length / 2);
+
+                return first.Equals(second);
+            }
     }
 }
